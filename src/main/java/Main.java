e994c282855e62cbdc39d3abe89fadacf460acfb
Naveennivas7 <naveennivaskrishna@gmail.com>
2026-07-14 17:14:07 +0530

    commited new things like put delete in the spring boot application

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
-public class Main {
-}
+//import dev.naveen.helloworld.TodoService;
+//
+//public class Main {
+//    public static void main(String []args){
+//        TodoService todoService=new TodoService();
+//        todoService.getTodo();
+//    }
+//}
--- src/main/java/dev/naveen/helloworld/TodoRepository.java
+++ src/main/java/dev/naveen/helloworld/TodoRepository.java
@@ -1,5 +1,9 @@
 package dev.naveen.helloworld;
 
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class TodoRepository {
     String getAllTodos(){
         return "Todos";
